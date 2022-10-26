bca3e1a512be33cea6a6aa34a18d7688228c1a56
reese_s <icipherfly@gmail.com>
2022-10-26 14:51:51 -0700

    initial commit.

---
+++ comments/code.js
@@ -0,0 +1,3 @@
+function callFunction(){
+   console.log ("comments lecture");
+}
---
+++ variables/code.js
@@ -0,0 +1,30 @@
+function calculateOrder(){
+   // DOM API Method to getElementById
+   var smallQuantityInput = document.getElementById("small");
+   var mediumQuantityInput = document.getElementById("medium");
+   var largeQuantityInput = document.getElementById("large");
+   // Value property by dot notation > .value
+   var numSmallPizzas = parseInt(smallQuantityInput.value, 10);
+   var numMediumPizzas = parseInt(mediumQuantityInput.value, 10);
+   var numLargePizzas = parseInt(largeQuantityInput.value, 10);
+
+   var priceSmall = 14.00;
+   var priceMedium = 18.00;
+   var priceLarge = 22.00;
+
+   var totalPizzas = numSmallPizzas + numMediumPizzas + numLargePizzas;
+
+   if (totalPizzas >= 5) {
+      priceSmall = 12.60;
+      priceMedium = 16.60;
+      priceLarge = 19.80;
+   }
+   
+   var totalCost = 
+      (numSmallPizzas * priceSmall) + 
+      (numMediumPizzas * priceMedium) + 
+      (numLargePizzas * priceLarge);
+
+   var totalInput = document.getElementById("total");
+   totalInput.value = totalCost.toFixed(2);
+}
