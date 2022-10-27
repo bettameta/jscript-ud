7a9d217704bf1b48050f1f416e47e9b60f9f380e
reese_s <icipherfly@gmail.com>
2022-10-27 09:53:03 -0700

    Freezing this section of the function before moving on.

---
+++ functions/code.js
@@ -0,0 +1,6 @@
+function showFullName(){
+   // DOM API Method to getElementById
+   var firstName = document.getElementById("f-name");
+   var lastName = document.getElementById("l-name");
+   console.log(firstName.value + " " + lastName.value);
+}
